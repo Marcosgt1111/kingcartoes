351aad41000503216ebccedbd37c7618aef49384
Marcosgtdev <Marcosgtdev>
2024-10-21 23:02:43 -0300

    STYLE: adicionando estilo e foto do produto main

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className="relative bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/images/header-bg.jpg')" }}>
+        <header className="relative bg-cover bg-center bg-no-repeat min-h-screen" style={{ backgroundImage: "url('/images/header-bg.jpg')" }}>
 
-            <div className="ml-4">
+            <div className="absolute top-0 left-0 ml-4 mt-3">
                 <Link
                     href="/">
                         <Image src="/images/logo.svg" alt="Logo" width={150} height={50}/>
@@ -13,9 +13,18 @@ const Header = () => {
             </div>
 
             <div className="text-header">
-                <h1 className="text-4xl font-bold text-yellow-400 text-center">Uma apresentação completa <br/> com o cartão correto.</h1>
+                <h1 className="text-6xl font-bold text-red-50 text-center py-5 drop-shadow-lg ">Uma apresentação completa <br/> com o cartão correto.</h1>
+            </div>
+
+            <div className="">
+                <Link
+                    href="/">
+                        <Image src="/images/cartaodevisita.png" alt="produto" width={150} height={50}/>
+                </Link>
             </div>
         </header>
+
+
     );
 };
 
