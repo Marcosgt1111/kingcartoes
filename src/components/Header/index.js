f212b6062f3725c319dfbc81b0ed1cfb03fe9c16
Marcosgtdev <Marcosgtdev>
2024-10-24 23:45:08 -0300

    STYLE: posicionado buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,12 +18,12 @@ const Header = () => {
                     completa <br/> com o cartão correto.</h1>
             </div>
 
-            <div>
-                <button
-                    className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"><span
-                    className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Pink to orange
-                    </span></button>
+            <div className= "absolute bottom-48 left-16 ml-96">
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Peça Já o seu!
+                    </span>
+                </button>
             </div>
 
             <div className="flex justify-center items-center min-h-screen">
