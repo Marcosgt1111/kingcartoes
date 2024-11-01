c84abd0620e9579ae22beb8b508945acab6ce684
Marcosgtdev <Marcosgtdev>
2024-10-31 22:56:43 -0300

    FEAT: adicionando main ao projeto

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,9 +2,10 @@
 
 const Main = () => {
     return (
-        <main>
-            <div>
-            </div>
+        <main className="bg-yellow-950">
+            <h1 className="text-xl font-bold leading-tight text-orange-700">
+                Ola parte 2
+            </h1>
         </main>
     );
 };
