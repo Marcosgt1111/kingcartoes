aa37260432d82eb539cd0adbd80d53e215617aaa
Marcosgtdev <Marcosgtdev>
2024-11-09 00:33:24 -0300

    FEAT: Adicionando banners dos produtos

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,12 +4,20 @@ import Image from "next/image";
 
 const Main = () => {
     return (
-        <main className="bg-gradient-to-r from-yellow-500 to-orange-900">
-            <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
-                    Cartões de Visita
+        <main className="bg-gradient-to-r from-amber-200 to-yellow-100">
+
+            <div className="p-14">
+                <h1 className="text-4xl font-bold text-amber-950 text-center text-white">
+                    Confira nossos produtos
                 </h1>
-                <p> á partir de R$ 80,00 </p>
+                <p className="text-2xl text-center mt-4">Melhor preço do Vale do Paraíba</p>
+            </div>
+
+            <div>
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                    Cartões de Visita
+                </h2>
+                <p> á partir de <strong>R$ 80,00</strong></p>
                 <div>
                     <Link href="">
                         <Image src="/images/cartao_cliente.png" alt="produto" width={500} height={50}/>
@@ -18,37 +26,37 @@ const Main = () => {
             </div>
 
             <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
                     Folhetos e Panfletos
-                </h1>
+                </h2>
                 <p> Ideal para seu negocio </p>
             </div>
 
             <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
                     Banners
-                </h1>
+                </h2>
                 <p> Convidar a clientela </p>
             </div>
 
             <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
                     Placas
-                </h1>
+                </h2>
                 <p> Uma fachada chamativa </p>
             </div>
 
             <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
                     Imãs de Geladeira
-                </h1>
+                </h2>
                 <p> Seu cliente merece um brinde </p>
             </div>
 
             <div>
-                <h1 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-xl font-bold leading-tight text-orange-700">
                     Blocos e Talões
-                </h1>
+                </h2>
                 <p> Controlar seu negocio </p>
             </div>
 
