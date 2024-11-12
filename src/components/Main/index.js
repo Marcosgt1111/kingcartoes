d225107584ac3dfe54b42d6bc93e96e5e21b55b2
Marcosgtdev <Marcosgtdev>
2024-11-11 21:52:40 -0300

    FEAT: Adicionando arquivos faltantes de produtos

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,61 +7,92 @@ const Main = () => {
         <main className="bg-gradient-to-r from-amber-200 to-yellow-100">
 
             <div className="p-14">
-                <h1 className="text-4xl font-bold text-amber-950 text-center text-white">
+                <h1 className="text-4xl font-bold  text-center text-black">
                     Confira nossos produtos
                 </h1>
                 <p className="text-2xl text-center mt-4">Melhor preço do Vale do Paraíba</p>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Cartões de Visita
                 </h2>
                 <p> á partir de <strong>R$ 80,00</strong></p>
+
                 <div>
                     <Link href="">
-                        <Image src="/images/cartao_cliente.png" alt="produto" width={500} height={50}/>
+                        <Image src="/images/cartao_cliente.png" alt="cartaodevisita" width={300} height={50}/>
                     </Link>
                 </div>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Folhetos e Panfletos
                 </h2>
                 <p> Ideal para seu negocio </p>
+
+                <div>
+                    <Link href="">
+                        <Image src="/images/panfleto.png" alt="panfleto" width={300} height={50}/>
+                    </Link>
+                </div>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Banners
                 </h2>
-                <p> Convidar a clientela </p>
+                <p> Divulge seus produtos </p>
+
+                <div>
+                    <Link href="">
+                        <Image src="/images/banner.png" alt="banner" width={300} height={50}/>
+                    </Link>
+                </div>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Placas
                 </h2>
                 <p> Uma fachada chamativa </p>
+
+                <div>
+                    <Link href="">
+                        <Image src="/images/placas.png" alt="placas" width={300} height={50}/>
+                    </Link>
+                </div>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Imãs de Geladeira
                 </h2>
                 <p> Seu cliente merece um brinde </p>
+
+                <div>
+                    <Link href="">
+                        <Image src="/images/ima.png" alt="ima" width={300} height={50}/>
+                    </Link>
+                </div>
             </div>
 
             <div>
-                <h2 className="text-xl font-bold leading-tight text-orange-700">
+                <h2 className="text-3xl font-bold leading-tight text-orange-700">
                     Blocos e Talões
                 </h2>
-                <p> Controlar seu negocio </p>
+                <p> Organize seu negocio </p>
+
+                <div>
+                    <Link href="">
+                        <Image src="/images/talao.png" alt="talao" width={300} height={50}/>
+                    </Link>
+                </div>
             </div>
 
             <div>
-                <p> Faça já seu Orçamento </p>
+                <p> Solicite seu Orçamento </p>
             </div>
 
         </main>
