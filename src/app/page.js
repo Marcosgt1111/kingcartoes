ef560b60d40c504c2a8339cc446d2b183d6d54c3
Marcosgtdev <Marcosgtdev>
2024-11-14 23:49:49 -0300

    FEAT: Adicionando Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Main from '@/components/Main';
+import Footer from '@/components/Footer';
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Footer =  () => {
+    return (
+        <h1>
+            Ola Pé no Zé
+        </h1>
+    )
+}
+
+export default Footer;
