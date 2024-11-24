886d42ebf67581d9b5a1f8f92cf15d71631eb663
Marcosgtdev <Marcosgtdev>
2024-11-24 13:14:47 -0300

    FEAT: Adicioanando imagens e textos no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,24 @@ import Image from "next/image";
 
 const Footer =  () => {
     return (
-        <h1>
-            Ola Pé no Zé
-        </h1>
+        <footer className="relative bg-cover bg-center bg-no-repeat min-h-screen"
+                style={{backgroundImage: "url('/images/marking_background.jpg')"}}>
+
+            <div className="mt-3 md:absolute md:top-16 md:right-0 md:mr-24 md:mt-3">
+                <Link href="/">
+                    <Image src="/images/marking_logo.svg" alt="Marking" width={350} height={150}/>
+                </Link>
+            </div>
+
+            <div className="text-red-500">
+                <ul>
+                    <li>Desenvolvimento de Sites</li>
+                    <li>Artes Digitais</li>
+                    <li>Criação de logomarcas</li>
+                </ul>
+            </div>
+
+        </footer>
     )
 }
 
