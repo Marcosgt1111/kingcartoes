a12e6598bea851ffcc24e6e43d033f1043de123b
Marcosgtdev <Marcosgtdev>
2024-12-13 22:27:25 -0300

    FEAT= Adicionando animaçao GSAP

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+"use client";
+
 import Header from '@/components/Header';
 import Main from '@/components/Main';
 import Footer from '@/components/Footer';
-
 
 export default function Home() {
     return (
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,43 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Registre o plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
 const Main = () => {
+    const sectionRefs = useRef([]);
+    sectionRefs.current = [];
+
+    useEffect(() => {
+        sectionRefs.current.forEach((section, index) => {
+            gsap.from(section, {
+                opacity: 0,
+                y: 50,
+                duration: 1,
+                ease: "power3.out",
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 80%", // Inicia a animação quando o topo da seção atinge 80% da viewport
+                    end: "bottom 20%", // Termina a animação quando o fundo da seção atinge 20% da viewport
+                    toggleActions: "play none none none", // Reproduz a animação na entrada
+                },
+            });
+        });
+    }, []);
+
+    const addToRefs = (el) => {
+        if (el && !sectionRefs.current.includes(el)) {
+            sectionRefs.current.push(el);
+        }
+    };
+
     return (
         <main className="bg-gradient-to-r from-amber-200 to-yellow-100">
-
             <div className="p-14">
                 <h1 className="text-4xl font-bold text-center text-black">
                     Confira nossos produtos
@@ -13,7 +46,7 @@ const Main = () => {
             </div>
 
             <div className="flex flex-col items-center">
-                <div className="flex flex-row items-center justify-center gap-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Cartões de Visita
@@ -22,12 +55,12 @@ const Main = () => {
                     </div>
                     <div className="mt-4">
                         <Link href="">
-                            <Image src="/images/cartao_cliente.png" alt="cartaodevisita" width={300} height={50}/>
+                            <Image src="/images/cartao_cliente.png" alt="cartaodevisita" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-8 mt-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8 mt-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Folhetos e Panfletos
@@ -37,12 +70,12 @@ const Main = () => {
                     </div>
                     <div className="mt-4 mb-12">
                         <Link href="">
-                            <Image src="/images/panfleto.png" alt="panfleto" width={300} height={50}/>
+                            <Image src="/images/panfleto.png" alt="panfleto" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-8 mt-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8 mt-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Banners
@@ -52,12 +85,12 @@ const Main = () => {
                     </div>
                     <div className="mt-4 mb-12">
                         <Link href="">
-                            <Image src="/images/banner.png" alt="banner" width={300} height={50}/>
+                            <Image src="/images/banner.png" alt="banner" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-8 mt-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8 mt-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Placas
@@ -67,12 +100,12 @@ const Main = () => {
                     </div>
                     <div className="mt-4 mb-12">
                         <Link href="">
-                            <Image src="/images/placas.png" alt="placas" width={300} height={50}/>
+                            <Image src="/images/placas.png" alt="placas" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-8 mt-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8 mt-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Imãs de Geladeira
@@ -82,12 +115,12 @@ const Main = () => {
                     </div>
                     <div className="mt-4 mb-12">
                         <Link href="">
-                            <Image src="/images/ima.png" alt="ima" width={300} height={50}/>
+                            <Image src="/images/ima.png" alt="ima" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-8 mt-8">
+                <div ref={addToRefs} className="flex flex-row items-center justify-center gap-8 mt-8">
                     <div className="text-center">
                         <h2 className="text-3xl font-bold leading-tight text-orange-700">
                             Blocos e Talões
@@ -97,7 +130,7 @@ const Main = () => {
                     </div>
                     <div className="mt-4 mb-12">
                         <Link href="">
-                            <Image src="/images/talao.png" alt="talao" width={300} height={50}/>
+                            <Image src="/images/talao.png" alt="talao" width={300} height={50} />
                         </Link>
                     </div>
                 </div>
@@ -106,13 +139,12 @@ const Main = () => {
             <div className="flex flex-row items-center justify-center mt-8 md:mt-16">
                 <button
                     className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                        <span
-                            className="relative px-14 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                            Solite seu Orçamento
-                        </span>
+                    <span
+                        className="relative px-14 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Solicite seu Orçamento
+                    </span>
                 </button>
             </div>
-
         </main>
     );
 };
